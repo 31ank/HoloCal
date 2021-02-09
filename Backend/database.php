eaab10ee017e0a11a2003c59be5536d897dda8d1
31ank <32846768+31ank@users.noreply.github.com>
2021-02-09 01:00:17 +0100

    fixed untracked files

--- Backend/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "";
-    $user = "";
-    $password = "";
-    $database_name = "";
-    $pdo = new PDO("mysql:host=$host;dbname=$database_name", $user, $password, array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ));
-?>
