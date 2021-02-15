822ea361afe4e268f6baea2187f9f30de37e1222
31ank <32846768+31ank@users.noreply.github.com>
2021-02-15 21:39:47 +0100

    added inefficient timezone selector

--- Backend/api.php
+++ Backend/api.php
@@ -2,8 +2,15 @@
     require 'database.php';
 
     if($_GET['entries'] == "curweek"){
-
-        $query =$pdo->prepare("SELECT * FROM streams JOIN members ON members.id = streams.member_id");
+        $newTimezone = $_GET['timezone'];
+        if (strpos($_GET['timezone'],'\\') !== false) { //first we check if the url contains the string 'en-us'
+            $newTimezone = str_replace('\\', '+', $_GET['timezone']); //if yes, we simply replace it with en
+        }
+        if($_GET['timezone'] != null){
+            $query =$pdo->prepare("SELECT first_name, last_name, stream_name, stream_time, ch_url, CONVERT_TZ(stream_date,'+00:00','".$newTimezone."') AS stream_date FROM streams JOIN members ON members.id = streams.member_id");
+        } else {
+            $query =$pdo->prepare("SELECT * FROM streams JOIN members ON members.id = streams.member_id");
+        }
         $query->execute();
     
     
--- Frontend/js/datafetcher.js
+++ Frontend/js/datafetcher.js
@@ -5,6 +5,12 @@ Date.prototype.addDays = function (days) {
     date.setDate(date.getDate() + days);
     return date;
 }
+
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+}
+
 var day = new Date();
 day.setHours(0,0,0,0);
 
@@ -13,7 +19,7 @@ let weekEnd = weekStart.addDays(6);
 let counter = 0;
 let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-$.getJSON('https://blankbit.net/holo/api/api.php?entries=curweek', function (data) {
+$.getJSON('https://holocal.tv/api/api.php?entries=curweek', function (data) {
     // loop through all 24 hours
     for(let date = weekStart; date <= weekEnd; date = date.addDays(1)){
         document.getElementById(weekdays[counter]).innerHTML = weekdays[counter] + ' ' + zeroPad(date.getMonth() + 1, 2) + '.' + zeroPad(date.getDate(), 2);
@@ -36,13 +42,12 @@ $.getJSON('https://blankbit.net/holo/api/api.php?entries=curweek', function (dat
             });
             newLine += '</td>'
         }
-        $('#timetable').append(newLine + '</td></tr>');
+        $('#timetableBody').append(newLine + '</td></tr>');
     }
     
 })
 .done(function(){
     $('#loadedData').show();
-    console.log("LOADED?");
 })
 .fail(function() {
     $('#failedData').show();
@@ -51,8 +56,59 @@ $.getJSON('https://blankbit.net/holo/api/api.php?entries=curweek', function (dat
     $('#loadingMessage').hide();
 });
 
-function refetchData(){
-    console.log("CHANGED");
+function refetchData(selectedTime){
+    var day = new Date();
+    day.setHours(0, 0, 0, 0);
+
+    let weekStart = (day.addDays(-(day.getDay() - 1)));
+    let weekEnd = weekStart.addDays(6);
+    let counter = 0;
+    let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    $("#timetableBody tr").remove()
+    $.getJSON('https://holocal.tv/api/api.php?entries=curweek&timezone=' + selectedTime.value, function (data) {
+        // loop through all 24 hours
+        for (let date = weekStart; date <= weekEnd; date = date.addDays(1)) {
+            document.getElementById(weekdays[counter]).innerHTML = weekdays[counter] + ' ' + zeroPad(date.getMonth() + 1, 2) + '.' + zeroPad(date.getDate(), 2);
+            counter++;
+        }
+        for (let index = 0; index < 24; index++) {
+            // Insert time
+            let newLine = '<tr>' + '<td>' + zeroPad(index, 2) + ':00</td>';
+            // loop through all days in a week
+            for (let date = weekStart; date <= weekEnd; date = date.addDays(1)) {
+                newLine += '<td>'
+                data.forEach(element => {
+                    let streamDate = new Date(element['streamDate']);
+                    let hoursCorrect = false;
+                    date.setHours(index);
+                    if(streamDate.getHours() == date.getHours()){
+                        hoursCorrect = true;
+                    }
+
+                    let dateDay = date;
+                    dateDay.setHours(0, 0, 0, 0);
+                    let streamDay = streamDate;
+                    streamDay.setHours(0, 0, 0, 0);
+                    if (hoursCorrect && streamDay.getTime() == dateDay.getTime()) {
+                        // new div element!
+                        newLine += '<a href="' + element['streamURL'] + '" target="_blank">' + element['member'] + '</a> ' + element['streamName'] + '<br>';
+                    }
+                });
+                newLine += '</td>'
+            }
+            $('#timetable').append(newLine + '</td></tr>');
+        }
+
+    })
+    .done(function () {
+        $('#loadedData').show();
+    })
+    .fail(function () {
+        $('#failedData').show();
+    })
+    .always(function () {
+        $('#loadingMessage').hide();
+    });
 }
 
 function debug() {
