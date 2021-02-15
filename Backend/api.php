d19f28a8981d5f9c9eb39305d7519dac56fc9dd7
31ank <32846768+31ank@users.noreply.github.com>
2021-02-15 21:41:14 +0100

    removed wrong comments

--- Backend/api.php
+++ Backend/api.php
@@ -3,8 +3,8 @@
 
     if($_GET['entries'] == "curweek"){
         $newTimezone = $_GET['timezone'];
-        if (strpos($_GET['timezone'],'\\') !== false) { //first we check if the url contains the string 'en-us'
-            $newTimezone = str_replace('\\', '+', $_GET['timezone']); //if yes, we simply replace it with en
+        if (strpos($_GET['timezone'],'\\') !== false) {
+            $newTimezone = str_replace('\\', '+', $_GET['timezone']);
         }
         if($_GET['timezone'] != null){
             $query =$pdo->prepare("SELECT first_name, last_name, stream_name, stream_time, ch_url, CONVERT_TZ(stream_date,'+00:00','".$newTimezone."') AS stream_date FROM streams JOIN members ON members.id = streams.member_id");
